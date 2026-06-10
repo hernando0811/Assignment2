f63dc34d80b5e366c2bf63de78c4440bdc9ea076
hernando0811 <hernanmartinez67@yahoo.com>
2026-06-10 13:51:39 -0400

    Modified the process_coins function

--- cashier.py
+++ cashier.py
@@ -6,6 +6,18 @@ class Cashier:
         """Returns the total calculated from coins inserted.
            Hint: include input() function here, e.g. input("how many quarters?: ")"""
         ###
+        print(" please insert coins for service ")
+        quarters = int(input("How many quarters?: "))
+        dimes = int(input("How many dimes?: "))
+        nickles = int(input("How many nickles?: "))
+        pennies = int(input("How many pennies?: "))
+
+        total_amount = quarters * 0.25
+        total_amount += dimes * 0.10
+        total_amount += nickles * 0.05
+        total_amount += pennies * 0.01
+
+        return total_amount
 
     def transaction_result(self, coins, cost):
         """Return True when the payment is accepted, or False if money is insufficient.
